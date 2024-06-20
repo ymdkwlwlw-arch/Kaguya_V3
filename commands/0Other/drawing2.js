7036bae6890a8b22daa87c7143664609d13c5c66
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-06-20 17:00:17 +0100

    Update drawing2.js

--- commands/0Other/drawing2.js
+++ commands/0Other/drawing2.js
@@ -17,7 +17,7 @@ export default {
       const translationResponse = await axios.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=ar&tl=en&dt=t&q=${encodeURIComponent(prompt)}`);
       const translatedPrompt = translationResponse?.data?.[0]?.[0]?.[0] || prompt;
 
-      const emiApiUrl = `https://samirxpikachu.onrender.com/animagine?prompt=${encodeURIComponent(translatedPrompt)}`;
+      const emiApiUrl = `https://gpt4withcustommodel.onrender.com/imagine?prompt=${encodeURIComponent(translatedPrompt)}`;
       const startTime = Date.now();
 
       const emiResponse = await axios.get(emiApiUrl, {
