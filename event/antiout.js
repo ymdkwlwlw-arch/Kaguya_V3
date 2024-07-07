9198bb8a504b67e22409ca7428c290ebf1e87d06
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-07-07 06:23:20 +0100

    Update antiout.js

--- event/antiout.js
+++ event/antiout.js
@@ -1,5 +1,12 @@
+import axios from 'axios';
+
 export default {
-  async handleEvent(api, event) {
+  name: "Re-add User",
+  author: "مشروع كاغويا",
+  role: "admin",
+  description: "إعادة إضافة عضو إلى المجموعة عند مغادرتهم.",
+  
+  execute: async function ({ api, event }) {
     // Exit early if the bot itself has left the group
     if (event.logMessageData?.leftParticipantFbId === api.getCurrentUserID()) return;
 
