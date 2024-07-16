7aa203a953233087d9b6eee0f16b8e33ee099629
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-07-16 03:56:15 +0100

    Update subscribe.js

--- event/subscribe.js
+++ event/subscribe.js
@@ -57,7 +57,7 @@ export default {
           const welcomeMessagePart2 = `✿━━━━━━━━━━━━━━━━✿\n ⚙️  | جاري توصيل ${botName} في المجموعة..... \n
 ❏ التاريخ : ${moment().tz("Africa/Casablanca").format("YYYY-MM-DD")}
 ❏ الوقت : ${moment().tz("Africa/Casablanca").format("HH:mm:ss")}
-\n✿━━━━━━━━━━━━━━━✿`;
+\n✿━━━━━━━━━━━━━━✿`;
 
           // إرسال رسالة الدخول
           const videoPath = "cache12/welcome.mp4";
