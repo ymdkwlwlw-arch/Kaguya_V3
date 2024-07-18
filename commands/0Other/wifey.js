f7d4411a5ad9a869b2ba99b8834104f80bb7158f
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-07-18 10:15:14 +0100

    Update wifey.js

--- commands/0Other/wifey.js
+++ commands/0Other/wifey.js
@@ -1,15 +1,5 @@
 import axios from "axios";
 
-async function translate(text) {
-  try {
-    const translationResponse = await axios.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ar&dt=t&q=${encodeURIComponent(text)}`);
-    return translationResponse?.data?.[0]?.[0]?.[0];
-  } catch (error) {
-    console.error('Translation Error:', error.message);
-    return text;
-  }
-}
-
 export default {
   name: "ملصق",
   author: "kaguya project",
@@ -19,21 +9,21 @@ export default {
     try {
       if (event.type === "message_reply") {
         const attachment = event.messageReply.attachments[0];
-        
+
         if (attachment.type === "sticker" || attachment.type === "photo") {
-          const itemID = attachment.ID;
-          return api.sendMessage({ body: itemID }, event.threadID);
+          const attachmentID = attachment.ID;
+          return api.sendMessage({ body: attachmentID }, event.threadID);
         } else {
-          return api.sendMessage(" ⚠️ | قم بالرد على ملصق أو صورة رمزية", event.threadID);
+          return api.sendMessage("⚠️ | قم بالرد على ملصق أو صورة رمزية", event.threadID);
         }
       } else if (args[0]) {
         return api.sendMessage({ sticker: args[0] }, event.threadID);
       } else {
-        return api.sendMessage(" فقط رد على الملصق أو الصورة الرمزية 😒", event.threadID);
+        return api.sendMessage("فقط رد على الملصق أو الصورة الرمزية 😒", event.threadID);
       }
     } catch (error) {
       console.error('Error:', error.message);
-      api.sendMessage("❌ |حدث خطأ أثناء معالجة طلبك.", event.threadID);
+      api.sendMessage("❌ | حدث خطأ أثناء معالجة طلبك.", event.threadID);
     }
   }
 };
