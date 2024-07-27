b6e4bbbe68a1c90a357a358e98cb396f7f55899f
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-07-28 00:28:07 +0100

    Update 0waifu.js

--- commands/0Other/0waifu.js
+++ commands/0Other/0waifu.js
@@ -9,7 +9,7 @@ export default {
   description: 'الحصول على صورة عشوائية لشخصية أنمي',
   async execute({ api, event }) {
     const cacheFolderPath = path.join(process.cwd(), 'cache');
-    const imagePath = path.join(cacheFolderPath, 'waifu_image.png');
+    const imagePath = path.join(cacheFolderPath, 'waifu_image.jpg');
 
     if (!fs.existsSync(cacheFolderPath)) {
       fs.mkdirSync(cacheFolderPath);
@@ -22,24 +22,24 @@ export default {
       const response = await axios.get('https://nash-api-end.onrender.com/waifu?search=waifu');
 
       if (response.data && response.data.images && response.data.images.length > 0) {
-        const imageUrl = response.data.images[0].url;
+        const imageUrl = response.data.images[0].preview_url; // استخدام preview_url
 
-        // Download the image as an arraybuffer
+        // تحميل الصورة كـ arraybuffer
         const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
         const imageBuffer = Buffer.from(imageResponse.data, 'binary');
 
-        // Save the image to the specified path
+        // حفظ الصورة في المسار المحدد
         await fs.outputFile(imagePath, imageBuffer);
 
         api.setMessageReaction('😘', event.messageID, (err) => {}, true);
 
-        // Send the image in a message
+        // إرسال الصورة في رسالة
         api.sendMessage({ attachment: fs.createReadStream(imagePath) }, tid, () => fs.unlinkSync(imagePath), mid);
       } else {
-        return api.sendMessage('❌ | فشل في جلب صورة عشوائية لشخصية أنمي. يرجى المحاولة مرة أخرى.', tid, mid);
+        return api.sendMessage('فشل في جلب صورة عشوائية لشخصية أنمي. يرجى المحاولة مرة أخرى.', tid, mid);
       }
     } catch (e) {
-      return api.sendMessage(`❌ | حدث خطأ أثناء جلب الصورة: ${e.message}`, tid, mid);
+      return api.sendMessage(e.message, tid, mid);
     }
-  }
+  },
 };
