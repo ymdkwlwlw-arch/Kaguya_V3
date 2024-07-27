1a9bdfd68381de23735bf3c30629c997f58a0c24
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-07-27 15:17:51 +0100

    Update 000hinata.js

--- commands/0Other/000hinata.js
+++ commands/0Other/000hinata.js
@@ -78,7 +78,7 @@ export default {
       // إرسال الصورة
       api.setMessageReaction("💞", event.messageID, () => {}, true);
       const message = {
-        body: `✿━━━━━━━━━━━━━━━━━✿\nصور هيناتا 💮 \n عدد الصور: ${animeImageLinks.length}\nتم الخصم منك 1000 دولار`,
+        body: `✿━━━━━━━━━━━━━━✿\nصور هيناتا 💮 \n عدد الصور: ${animeImageLinks.length}\nتم الخصم منك 100 دولار\n✿━━━━━━━━━━━━━━✿`,
         attachment: fs.createReadStream(tempImagePath),
       };
       api.sendMessage(message, event.threadID, () => {
