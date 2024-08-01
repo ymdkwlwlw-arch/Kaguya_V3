bc765aac8ef9fbd07739625a602aa8a6212c5a36
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-08-01 15:42:33 +0100

    Update admin.js

--- commands/Admin/admin.js
+++ commands/Admin/admin.js
@@ -1,10 +1,10 @@
-import fs from "fs";
+import fs from 'fs';
 
 class Admin {
   name = "آدمن";
   author = "Kaguya Project";
   cooldowns = 60;
-  description = "إضافة أو إزالة أو تعيين رتبة المسؤول";
+  description = "إضافة أو إزالة أو تعيين رتبة المسؤول وتفعيل أو تعطيل البوت";
   role = "member";
   aliases = [];
 
@@ -13,7 +13,7 @@ class Admin {
       global.client.__proto__.setConfig = function (newConfig) {
         try {
           Object.assign(global.client.config, newConfig);
-          fs.writeFileSync("./setup/config.js", `export default ${JSON.stringify(global.client.c, null, 2)};`);
+          fs.writeFileSync("./setup/config.json", JSON.stringify(global.client.config, null, 2));
         } catch (err) {
           this.emit("system:err", err);
         }
@@ -23,28 +23,35 @@ class Admin {
 
   async execute({ event, args }) {
     try {
-      var [type, tags] = args;
-      tags = event.mentions && Object.keys(event.mentions).length > 0 ? event.mentions : tags && !isNaN(tags) ? { [tags]: "" } : false;
+      const [type, tags] = args;
+      const isAdmin = global.client.config.ADMIN_IDS.includes(event.senderID);
+      const isAdminCommand = ["تفعيل", "تعطيل", "إضافة", "إزالة", "قائمة"].includes(type);
 
-      if (["add", "remove"].includes(type) && !global.client.config.ADMIN_IDS.includes(event.senderID)) {
+      if (!isAdmin && !["قائمة", "list"].includes(type)) {
         return kaguya.reply(" ⚠️ | ليس لديك الإذن لاستخدام هذا الأمر!");
       }
 
-      switch (type) {
-        case "إضافة":
+      if (isAdminCommand) {
+        if (type === "تفعيل") {
+          global.client.setConfig({ botEnabled: true });
+          return kaguya.reply(" ✅ | تم تفعيل تقييد إستخدام البوت بنجاح!");
+        }
+        if (type === "تعطيل") {
+          global.client.setConfig({ botEnabled: false });
+          return kaguya.reply(" ❌ | تم تعطيل تقييد إستخدام البوت بنجاح!");
+        }
+        if (type === "إضافة") {
           return this.addAdmin(tags);
-
-        case "إزالة":
+        }
+        if (type === "إزالة") {
           return this.removeAdmin(tags);
-
-        case "قائمة":
-        case "-l":
-        case "-all":
+        }
+        if (type === "قائمة" || type === "-l" || type === "-all") {
           return this.listAdmins();
-
-        default:
-          var commandName = client.config.prefix + this.name;
-          return kaguya.reply(`[ آدمن ]\n${commandName} إضافة <@منشن أو الآيدي> قم بإضافة العضو آدمن على البوت \n${commandName} إزالة <@منشن أو الآيدي> قم بإزالة العضو من دور الآدمن على البوت \n${commandName} قائمة إظهار قائمة الآدمنية على البوت`);
+        }
+      } else {
+        var commandName = client.config.prefix + this.name;
+        return kaguya.reply(`[ آدمن ]\n${commandName} تفعيل لتفعيل البوت\n${commandName} تعطيل لتعطيل البوت\n${commandName} إضافة <@منشن أو الآيدي> لإضافة العضو كآدمن\n${commandName} إزالة <@منشن أو الآيدي> لإزالة العضو من قائمة الآدمنية\n${commandName} قائمة لإظهار قائمة الآدمنية`);
       }
     } catch (err) {
       console.log(err);
@@ -53,22 +60,22 @@ class Admin {
 
   addAdmin(tags) {
     if (!tags) {
-      return kaguya.reply(` ⚠️ | يرحى عمل منشن أو إدخال آيدي العضو المراد إضافته كآدمن`);
+      return kaguya.reply(` ⚠️ | يرجى عمل منشن أو إدخال آيدي العضو المراد إضافته كآدمن`);
     }
 
     const addedUids = this.processAdmins(tags, "add");
-    const statusMessage = this.getStatusMessage(addedUids, "");
+    const statusMessage = this.getStatusMessage(addedUids, "إضافة");
 
     return kaguya.reply(statusMessage);
   }
 
   removeAdmin(tags) {
     if (!tags) {
-      return kaguya.reply(` ⚠️ | يرحى عمل منشن أو إدخال آيدي العضو المراد إزالته من قائمة الآدمنية`);
+      return kaguya.reply(` ⚠️ | يرجى عمل منشن أو إدخال آيدي العضو المراد إزالته من قائمة الآدمنية`);
     }
 
     const removedUids = this.processAdmins(tags, "remove");
-    const statusMessage = this.getStatusMessage(removedUids, "Xoá");
+    const statusMessage = this.getStatusMessage(removedUids, "إزالة");
 
     return kaguya.reply(statusMessage);
   }
@@ -77,7 +84,7 @@ class Admin {
     const adminIds = global.client.config.ADMIN_IDS;
 
     if (adminIds.length === 0) {
-      return kaguya.reply(" ⚠️ | هذا العضو لم يتم إضافته إلى قائمة الآدمنية.");
+      return kaguya.reply(" ⚠️ | لا يوجد أعضاء في قائمة الآدمنية.");
     }
 
     const adminList = adminIds.join(", ");
@@ -88,7 +95,7 @@ class Admin {
     const uidsToProcess = Object.keys(tags);
     const processedUids = [[], []];
 
-    for (var uid of uidsToProcess) {
+    for (const uid of uidsToProcess) {
       if ((action === "add" && global.client.config.ADMIN_IDS.includes(uid)) || (action === "remove" && !global.client.config.ADMIN_IDS.includes(uid))) {
         processedUids[0].push(uid);
       } else {
@@ -106,14 +113,14 @@ class Admin {
     const [failedUids, successUids] = processedUids;
     const status = successUids.length > 0 ? " ✅ | نجحت" : "❌ | فشلت";
 
-    let message = `${status} عملية التعيين`;
+    let message = `${status} عملية ${action}`;
 
     if (successUids.length > 0) {
-      message += `\n\nتمت إضافة العضو صاحب الآيدي  ${action.toLowerCase()}\n ${successUids.join(", ")} نجحت بذالك عملية التعيين`;
+      message += `\n\nتمت ${action} العضو صاحب الآيدي: ${successUids.join(", ")}`;
     }
 
     if (failedUids.length > 0) {
-      message += `\n\n ⚠️ | موجود بالفعل في قائمة الآدمنية : ${failedUids.join(", ")} فشلت بذالك عملية التعيين`;
+      message += `\n\n ⚠️ | موجود بالفعل في قائمة الآدمنية: ${failedUids.join(", ")}`;
     }
 
     return message;
