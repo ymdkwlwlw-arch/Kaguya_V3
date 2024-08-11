e8e96d5e55c9a5369287ac15af733bcd50c50d9a
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-08-11 02:32:16 +0100

    Create Zara.js

---
+++ commands/0Other/Zara.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import fs from 'fs';
+
+async function getGreetingImage() {
+  // تحديد المسار إلى مجلد 'ara.mp3'
+  const imagePath = path.join(process.cwd(), 'cache12', 'sista.mp4'); // تحديث اسم الصورة إذا كان مختلفًا
+  return fs.createReadStream(imagePath);
+}
+
+export default {
+  name: "ارارار",
+  author: "البوت",
+  role: "member",
+   aliases:["اررر","ارارارا"],
+  description: "يرسل رسالة ترحيبية مع صورة.",
+  execute: async function({ api, event }) {
+    try {
+      const greetingImageStream = await getGreetingImage();
+       
+       api.setMessageReaction("🤭", event.messageID, (err) => {}, true);
+  
+      api.sendMessage({
+        body: "✧───────────────✧\n\t\t\t\tlove you 💖\n✧───────────────✧",
+        attachment: greetingImageStream
+      }, event.threadID, event.messageID);
+    } catch (error) {
+      console.error('Error sending greeting message:', error);
+      api.sendMessage('❌ | حدث خطأ أثناء إرسال الرسالة الترحيبية.', event.threadID, event.messageID);
+    }
+  }
+};
