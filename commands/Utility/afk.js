6dc1430d8cfcf816022256dcb9d57079b02a4ccd
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-08-19 11:40:57 +0100

    Update afk.js

--- commands/Utility/afk.js
+++ commands/Utility/afk.js
@@ -2,25 +2,38 @@ class Afk {
   name = "مشغول";
   author = "Kaguya Project";
   cooldowns = 10;
-  description = "يرسل البوت تقرير إليك في حالة تم عمل منشن لك أو تحدث عليك شخص ما.";
+  description = "قم بضبط وضع مشغول في حالة تم عمل منشن لك البوت سيخبرك";
   role = "member";
-  dataUser = {};
-
-  async execute({ api, event, args, Users }) {
-    var reason = args.join(" ") || "بدون سبب";
-    try {
-      var nameUser = (await Users.find(event.senderID))?.data?.data?.name || event.senderID;
-      this.dataUser[event.senderID] = { reason, nameUser, tag: [] };
-      return api.sendMessage(`قام بتشغيل ميزة عدم الإزعاج والسبب : ${reason}`, event.threadID, event.messageID);
-    } catch (err) {
-      console.log(err);
+  dataUser = {}
+  lang = {
+    "vi_VN": {
+      "afk": "Đã đặt trạng thái afk với lý do: $1",
+      "afk_reply": "Người dùng $1 đang afk với lý do: $2",
+      "afk_clear": "Chào mừng quay trở lại, những người đã tag bạn khi bạn afk:\n\n$1"
+    },
+    "ar_MA": {
+      "afk": " قام بتشغيل وضع عدم الإزعاج مع السبب : $1",
+      "afk_reply": "المستخدم  $1 هو مشغول والسبب : $2",
+      "afk_clear": "مرحبا بعودتك الأشخاص اللذين قامو بعمل منشن لك عندما كنت غابدئبا هم كالتالي:\n\n$1"
     }
   }
+  async execute({ api, event, args, Users }) {
+    var reason = args.join(" ") || client.config.language == "vi_VN" ? "Không có lý do" : "No reason";
+    try {
 
+      var nameUser = (await Users.find(event.senderID))?.data?.data?.name || event.senderID;
+      this.dataUser[event.senderID] = { reason, nameUser, tag: [] };
+      return api.sendMessage(getLang("plugins.afk.afk", reason), event.threadID, event.messageID);
+      
+    } catch (err) {
+      console.log(err)
+    }
+  }
   async events({ event, api }) {
     try {
+
       if (event.senderID in this.dataUser) {
-        return api.sendMessage(`مرحبا الأشخاص اللذين قامو بعمل منشن حين كنت غائبا هم كالتالي :\n\n${this.dataUser[event.senderID].tag.join(`\n` + '-'.repeat(30) + "\n")}`, event.threadID, () => {
+        return api.sendMessage(getLang("plugins.afk.afk_clear", this.dataUser[event.senderID].tag.join(`\n` + '-'.repeat(30) + "\n")), event.threadID, () => {
           delete this.dataUser[event.senderID];
         }, event.messageID);
       }
@@ -29,12 +42,13 @@ class Afk {
 
       for (let id of Object.keys(event.mentions)) {
         if (id in this.dataUser) {
-          this.dataUser[id].tag.push(`الآيدي 🧿 : ${event.senderID}\nمحتوى 📝 : ${event.body}\nوقت ⏰ : ${new Date().toLocaleString("ar-EA", { timeZone: "Africa/Casablanca" })}`);
-          api.sendMessage(`المستخدم ${this.dataUser[id].nameUser} مشغول مع السبب : ${this.dataUser[id].reason}`, event.threadID, event.messageID);
+          this.dataUser[id].tag.push(` 📎 | المعرف : ${event.senderID}\n نوي نانوغرام: ${event.body}\n📅 | التاريخ : ${new Date().toLocaleString("vi-VN", { timeZone: "Africa/Casablanca" })}`);
+          api.sendMessage(getLang("plugins.afk.afk_reply", this.dataUser[id].nameUser, this.dataUser[id].reason), event.threadID, event.messageID);
         }
       }
+
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
   }
 }
