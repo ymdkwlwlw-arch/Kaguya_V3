3b41c1ee828b1cd388d2b42c91bbef75cdf92693
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-08-19 11:48:10 +0100

    Update filterUsers.js

--- commands/Admin/filterUsers.js
+++ commands/Admin/filterUsers.js
@@ -12,19 +12,28 @@ class LocBox {
 
   async execute({ api, event, Threads, args }) {
     try {
-      const [length] = args.map(Number);
+      // الحصول على بيانات المجموعة الحالية والتحقق من صلاحيات البوت
+      const threadData = (await Threads.find(event.threadID))?.data?.data;
+      if (!threadData.adminIDs.includes(api.getCurrentUserID())) {
+        return api.sendMessage(" ⚠️ | يحتاج البوت أن يكون آدمن لإستخدام هذه الميزة", event.threadID);
+      }
 
+      // التحقق من صحة الرقم المدخل
+      const [length] = args.map(Number);
       if (isNaN(length) || length <= 0) {
         return kaguya.reply(" ⚠️ | ادخل عدد الأعضاء المراد تصفيتهم");
       }
 
+      // البحث عن المجموعات التي تحتوي على أقل من العدد المحدد من الأعضاء
       const threads = (await Threads.getAll()).data;
       const findThreads = threads.filter((thread) => thread.data.members < length);
 
+      // إذا لم يتم العثور على أي مجموعة تتطابق مع الشرط
       if (!findThreads.length) {
         return kaguya.reply(` ⚠️ | لم يتم إيجاد في المجموعة اقل من ${length} عضو`);
       }
 
+      // إزالة البوت من المجموعات التي تحتوي على أقل من العدد المحدد من الأعضاء
       for (const threadData of findThreads) {
         await api.removeUserFromGroup(api.getCurrentUserID(), threadData.threadID);
         await sleep(1000);
