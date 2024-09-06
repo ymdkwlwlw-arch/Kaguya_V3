357ea4b62639e656fc704c84ae36a8d0871e2d80
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-09-06 22:40:35 +0100

    Create lock.js

---
+++ commands/Admin/lock.js
@@ -0,0 +1,24 @@
+async function execute({ api, event, args }) {
+  const command = args[0];
+  
+  if (command === "تشغيل") {
+    autoReAddEnabled = true;
+    api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+  
+    await api.sendMessage(" ✅ | تم تفعيل ميزة عدم المغادرة", event.threadID);
+  } else if (command === "إيقاف") {
+    autoReAddEnabled = false;
+    api.setMessageReaction("🚫", event.messageID, (err) => {}, true);
+  
+    await api.sendMessage(" 🚫 | تم تعطيل ميزة عدم مغادرة المجموعة", event.threadID);
+  } else {
+    await api.sendMessage(" ⚠️ | يرجى استخدام إما 'تشغيل' أو 'إيقاف' للتحكم في الميزة.", event.threadID);
+  }
+}
+
+export default {
+  name: "قفل",
+  role:"admin",
+  description: "تشغيل أو إيقاف ميزة إعادة الأعضاء الذين يغادرون.",
+  execute,
+};
