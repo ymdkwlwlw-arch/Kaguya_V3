ce21be8aafa1896a9191497959de715fb1b63268
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-09-07 01:56:02 +0100

    Update colorize.js

--- commands/0Other/colorize.js
+++ commands/0Other/colorize.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { getStreamFromURL } from global.utils;
+import fs from "fs";
+import path from "path";
+import stream from "stream";
 
 export default {
   name: "غني",
@@ -26,14 +28,37 @@ export default {
       const url = `https://smfahim.onrender.com/ytb/audio?search=${encodeURIComponent(songName)}`;
       const { data: { audioUrl, title } } = await axios.get(url);
 
-      // Send song details with audio attachment
+      // Define file path to save the song locally
+      const filePath = path.join(process.cwd(), "cache", "music.mp3");
+      const writer = fs.createWriteStream(filePath);
+
+      // Download the song and save it to the local file system
+      const response = await axios({
+        url: audioUrl,
+        method: "GET",
+        responseType: "stream"
+      });
+
+      // Pipe the response stream to the file writer
+      response.data.pipe(writer);
+
+      // Wait until the download finishes
+      await new Promise((resolve, reject) => {
+        writer.on("finish", resolve);
+        writer.on("error", reject);
+      });
+
+      // Send song details with audio attachment from the local file
       await api.sendMessage({
         body: title,
-        attachment: await getStreamFromURL(audioUrl, "music.mp3")
+        attachment: await getStreamFromPath(filePath)
       }, threadID);
 
       // Set success reaction
       api.setMessageReaction("✅", messageID, () => {}, true);
+
+      // Delete the file after sending
+      fs.unlinkSync(filePath);
 
     } catch (error) {
       // Log error and send error message
