07a5e3cc47517f90d14531a9a3b647d3f990a873
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-09-25 17:53:20 +0100

    Update kaguya.js

--- utils/kaguya.js
+++ utils/kaguya.js
@@ -1,1 +1,19 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'utils')));
+
+app.get('/', async (_, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Bot is running on port ' + port);
+});
